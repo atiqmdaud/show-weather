e0857089b17bc61e855b9f63772338f6403865c1
atiqmdaud <atiqmdaud@gmail.com>
2024-11-29 14:51:46 +0800

    added responsiveness cointainer @media

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,46 @@
 <script setup lang="ts">
-import DemoOnly from '@/components/DemoOnly.vue'
+// import DemoOnly from '@/components/DemoOnly.vue'
 
 // import store from '@/store'
 </script>
 
 <template>
   <main>
-    <DemoOnly />
+    <div class="container">123</div>
   </main>
 </template>
+
+<style lang="css" scoped>
+.container {
+  background-color: #c3c3c3;
+  height: 100vh;
+}
+
+/* xs */
+@media (min-width: 475px) {
+  .container {
+    max-width: 475px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+/* sm */
+/* @media (min-width: 640px) {} */
+
+/* md */
+@media (min-width: 768px) {
+  .container {
+    max-width: 768px;
+  }
+}
+
+/* lg */
+/* @media (min-width: 1024px) {} */
+
+/* xl */
+/* @media (min-width: 1280px) {} */
+
+/* 2xl */
+/* @media (min-width: 1536px) {} */
+</style>
