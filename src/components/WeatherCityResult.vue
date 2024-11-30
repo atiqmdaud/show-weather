c75bd501e4c9d359d3e7af84c99a887aa794fd9f
atiqmdaud <atiqmdaud@gmail.com>
2024-11-30 09:41:28 +0800

    added weather city result component

---
+++ src/components/WeatherCityResult.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+div {
+  background-color: aquamarine;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import UserInput from '@/components/UserInput.vue'
 import WeatherAddedCard from '@/components/WeatherAddedCard.vue'
+import WeatherCityResult from '@/components/WeatherCityResult.vue'
 
 // import store from '@/store'
 </script>
@@ -9,7 +10,10 @@ import WeatherAddedCard from '@/components/WeatherAddedCard.vue'
   <section id="user-input">
     <UserInput />
   </section>
-  <section id="weather-list-card">
+  <section v-if="true" id="weather-city-result">
+    <WeatherCityResult />
+  </section>
+  <section v-else id="weather-list-card">
     <WeatherAddedCard />
     <WeatherAddedCard />
     <WeatherAddedCard />
@@ -27,13 +31,24 @@ import WeatherAddedCard from '@/components/WeatherAddedCard.vue'
 
 <style lang="css" scoped>
 section {
-  padding-left: 13px;
-  padding-right: 13px;
+  /* padding-left: 13px; */
+  /* padding-right: 13px; */
 }
 
+section#weather-city-result {
+  max-height: 270px;
+  /* overflow-y: scroll; */
+  overflow: auto;
+  /* background-color: antiquewhite; */
+  /* height: 370px; */
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  border: 1px solid #ccc;
+}
 section#weather-list-card {
-  max-height: 400px;
-  overflow-y: scroll;
+  max-height: 370px;
+  overflow-y: auto;
   display: flex;
   flex-direction: column;
   gap: 10px;
