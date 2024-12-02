6cec3d038adef91e5599dec64936c3dffadc35c6
atiqmdaud <atiqmdaud@gmail.com>
2024-12-03 01:13:24 +0800

    profile is done but state is local so it will lose if do routing

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -7,31 +7,68 @@
       <div class="top-btn-forward">btn forward</div>
     </div>
     <div class="second">
-      <img :src="photoUrl" alt="Profile Photo" v-if="photoUrl" />
-      <input type="file" @change="handleFileUpload" />
-      <label for="photo">Profile Photo:</label>
-      <div>name | phone number</div>
+      <!-- <img :src="photoUrl" alt="Profile Photo" v-if="photoUrl" /> -->
+      <img src="https://placehold.co/10" alt="profile photo" width="60px" height="60px" />
+      <div>
+        <input type="file" :disabled="disabled" />
+      </div>
+      <!-- <input type="file" @change="handleFileUpload" /> -->
+      <div>{{ name }}</div>
+      <div>{{ email }} | {{ phone }}</div>
     </div>
-    <div class="third">
+    <div>
       <form @submit.prevent="">
-        <div>
-          <label for="name">Name:</label> <input v-model="name" type="text" id="name" required />
-        </div>
-        <div>
-          <label for="email">Email:</label>
-          <input v-model="email" type="email" id="email" required />
-        </div>
-        <div>
-          <label for="phone">Phone Number:</label>
-          <input v-model="phone" type="tel" id="phone" required />
+        <div class="third">
+          <div>
+            <!-- <label for="name">Full name:</label> -->
+            <input v-model="name" type="text" id="name" required :disabled="disabled" />
+          </div>
+          <div>
+            <!-- <label for="email">Email:</label> -->
+            <input v-model="email" type="email" id="email" required :disabled="disabled" />
+          </div>
+          <div>
+            <!-- <label for="phone">Phone Number:</label> -->
+            <input v-model="phone" type="tel" id="phone" required :disabled="disabled" />
+          </div>
         </div>
       </form>
     </div>
     <div class="bottom">
-      <button type="submit">Edit/Submit</button>
+      <button @click="handleClick" type="submit">
+        {{ disabled ? 'EDIT' : 'SUBMIT' }}
+      </button>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const name = ref('demo')
+const email = ref('demo@demo.com')
+const countryCode = ref('+60')
+const phone = ref('123456789')
+const disabled = ref(true)
+
+const handleClick = () => {
+  if (disabled.value) {
+    handleDisabled()
+  } else {
+    handleSubmit()
+  }
+}
+
+const handleDisabled = () => {
+  console.log('clicked btn disabled')
+  disabled.value = false
+}
+
+const handleSubmit = () => {
+  name.value = name.value
+
+  disabled.value = true
+}
+</script>
 
 <style>
 .profile {
@@ -41,6 +78,7 @@
   flex-direction: column;
   /* align-items: center; */
   /* justify-content: center; */
+  margin: 15px;
 }
 
 .top {
@@ -55,6 +93,71 @@
 
 .top-btn-forward {
   visibility: hidden;
+}
+
+.second {
+  /* border: 1px solid red; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  /* text-align: center; */
+}
+
+.second img {
+  max-width: 100%;
+  height: auto;
+}
+
+/* styles.css */
+/* body {
+    font-family: Arial, sans-serif;
+} */
+
+.third {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  /* justify-content: center; */
+  /* align-items: center; */
+  /* border: 1px solid red; */
+  /* padding: 20px; */
+}
+.third input {
+}
+
+.bottom {
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+}
+
+input {
+  outline: none;
+  width: 100%;
+  box-sizing: border-box;
+  height: 40px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: white;
+}
+
+button {
+  width: 100%;
+  height: 40px;
+  background-color: #2e3a5a;
+  color: aliceblue;
+  border-radius: 5px;
+  font-size: 14px;
+  border: none;
+}
+
+button:active {
+  outline: none;
+  box-shadow: none;
+  transform: none;
 }
 @media (min-width: 1024px) {
 }
